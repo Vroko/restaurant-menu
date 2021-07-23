06d0e38063852e0ab0d6bcef2223f7f3ba2bde44
Jonathan Turnage <jetx2turnage@gmail.com>
2021-07-23 18:58:36 +0000

    Populating data

--- public/js/viewItems.js
+++ public/js/viewItems.js
@@ -1,11 +1,21 @@
+let cardsData = "";
+
 document.addEventListener('DOMContentLoaded', () => {
     getItems();
+    var app = new Vue({
+        el: '#app',
+        data: {
+          cardData: cardsData
+        }
+      })
+    console.log(cardsData);
 })
 
 const getItems = () => {
     const menuRef = firebase.database().ref('/menu');
     menuRef.on('value', (snapshot) => {
         const data = snapshot.val();
-        return data
+        cardsData = data;
     })
 }
+
